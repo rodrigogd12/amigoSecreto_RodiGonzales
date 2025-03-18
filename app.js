eacf3b776d081dbffcd6f7bae9ad4f400e61fc2c
Rodrigo Gonzales Dolz <52252456+rodrigogd12@users.noreply.github.com>
2025-03-18 13:17:23 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,45 +1,43 @@
 
-
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-// Array para almacenar nombres
+// Almacenar nombres
 let amigos = [];
 
-// funcion para agregar amigos
+// Agregar amigos
 function agregarAmigo(){
     const inputAmigo = document.getElementById('amigo');
     const nombreAmigo = inputAmigo.value.trim();
 
-//validar que el campo no este vacio
+// Campo no este vacio
 if(nombreAmigo === ""){
     alert("El campo no puede estar vacio, ingrese un nombre");
     return;
 }
 
-//que no se repitan nombres
+// No se repitan nombres
 if(amigos.includes(nombreAmigo)){
     alert(`Nombre repetido ${nombreAmigo} agregue otro nombre`);
     return;
 }
 
-//Agregar nombre
+// Agregar el nombre
 amigos.push(nombreAmigo);
 
-//Borrar lista de amigos
+// Borrar lista de los amigos
 inputAmigo.value = "";
 
-//Actualizar lista
+// Actualizar lista de amigos
 actualizarLista();
 
 }
 
-//Funcion amigos en interfaz
+// Amigos en la interfaz
 function actualizarLista(){
     const listaAmigos = document.getElementById('listaAmigos');
 
-//limpiar contenido actual de la lista
+// limpiar contenido de la lista de amigos
 listaAmigos.innerHTML = "";
 
-//recorrer lista con un for
+// Un for para recorrer lista de amigos
 for(let i = 0; i < amigos.length; i++){
     const li = document.createElement('li');
     li.textContent = amigos[i];
@@ -47,21 +45,21 @@ for(let i = 0; i < amigos.length; i++){
 }
 }
 
-//Funcion para seleccionar amigo de manera aleatoria
+// Seleccionar amigo de manera aleatoria
 function sortearAmigo(){
-//Validar sddi hay amigos 
+// Validar si hay amigos 
     if(amigos.length === 0){
         alert('No hay amigos, Agrega un amigo al menosd');
         return;
 }
 
-//Genera el amigo aleatorio
+// Generar amigo aleatorio
 const indiceAleatorio = Math.floor(Math.random() * amigos.length);
 
-//obtener el amigo aleatorio
+// obtener el amigo aleatorio
 const amigoSorteado = amigos[indiceAleatorio];
 
-//Mosdtrar resultado
+// Mosdtrar resultado
 const resultado = document.getElementById('resultado');
 resultado.innerHTML = `El amigo que salio es: <strong>${amigoSorteado}</strong>`
 
